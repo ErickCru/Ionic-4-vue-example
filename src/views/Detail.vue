f3fa2acbb376740671fb3cd58c3570dd8ebc9563
Erick <cruzperick@gmail.com>
2019-06-17 19:32:11 -0500

    Agregado los archivos.

---
+++ src/views/Detail.vue
@@ -0,0 +1,31 @@
+<template>
+  <ion-card>
+    <ion-card-header>
+      <ion-label>{{post.title}}</ion-label>
+    </ion-card-header>
+    <ion-card-content>
+      <template v-if="image.startsWith('https://')">
+        <ion-img :src="image"></ion-img>
+      </template>
+      <template v-else>
+        <ion-lab color="danger">No tiene imagen</ion-lab>
+      </template>
+      <ion-button color="tertiary" expand="full" @click="$router.go(-1)" style="margin-top: 1rem">Go Back</ion-button>
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script>
+export default {
+  props: ["post"],
+  data() {
+    return {
+      image: ""
+    };
+  },
+  mounted() {
+    this.image = this.post.preview.images[0].source.url.replace(/&amp;/g, "&");
+  }
+};
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ion-card v-for="post in posts" :key="post.data.id" style="border: 1px solid blue">
+      <ion-card-content>
+        <template v-if="post.data.thumbnail.startsWith('https://')">
+        <img :src="post.data.thumbnail" alt="" style="margin-bottom: 1rem">
+        </template>
+        <ion-label color="light">
+          {{post.data.title}}
+        </ion-label>
+        <ion-button color="tertiary" expand="full" @click="viewMore(post.data)" style="margin-top: 1rem"> 
+          View more
+        </ion-button>
+      </ion-card-content>
+    </ion-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
+  export default {
+    data() {
+      return {
+        posts: []
+      }
+    },
+    async mounted() {
+      const response = await axios.get('https://www.reddit.com/r/marvelstudios.json');
+     this.posts = response.data.data.children;
+     console.log(this.posts)
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+    },
+    methods: {
+      viewMore(post){
+        this.$router.push({name: 'detail', params: {post}});
+      }
+    },
   }
-}
 </script>
+
